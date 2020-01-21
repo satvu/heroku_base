3c5a2552d1dc592c002cf24cbb5385c51a51f0a2
satvu <sarahvu@mit.edu>
2020-01-20 22:37:27 -0600

    edit main page and start adding menu page

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+# Menu Items
+class MenuItem(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.TextField()
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Greeting, MenuItem
 
 # Create your views here.
 def index(request):
@@ -17,3 +17,8 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def menu(request):
+    menuItems = MenuItem.objects.all()
+
+    return render(request, "menu.html", {"menu_items": menuItems})
