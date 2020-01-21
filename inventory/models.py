49b76f657c9db5dd58350177fc988f0333933237
satvu <sarahvu@mit.edu>
2020-01-21 08:51:07 -0600

    reverse order of models

--- inventory/models.py
+++ inventory/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
 
+# Ingredients - have a name and count of it in the inventory
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    amount = models.IntegerField()
+    
 # Menu Items
 class MenuItem(models.Model):
     name = models.CharField(max_length=30)
@@ -11,7 +16,3 @@ class MenuItem(models.Model):
     description = models.TextField()
     image = models.TextField()
     ingredients = models.ManyToManyField(Ingredient)
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=30)
-    amount = models.IntegerField()
