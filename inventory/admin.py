da8e83478588dd390d67516881a47cd95bb87fc0
satvu <sarahvu@mit.edu>
2020-01-21 14:21:43 -0600

    edit

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,10 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Ingredient)
-class Ingredient(admin.ModelAdmin):
-    pass
-
-@admin.register(MenuItem)
-class MenuItem(admin.ModelAdmin):
-    pass
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
