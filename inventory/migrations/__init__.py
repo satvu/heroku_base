dae49def3f5a6052d0eb02174e5e71f419ceb5fb
satvu <sarahvu@mit.edu>
2020-01-22 09:01:01 -0600

    test mig" g

---
+++ inventory/migrations/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [('migrations', '0001_initial')]
+
+    operations = [
+    ]
