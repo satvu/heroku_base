869b49155ec4b7465436fceb972d28e5378cb812
satvu <sarahvu@mit.edu>
2020-01-22 13:25:07 -0600

    reverting

--- inventory/migrations/0001_initial.py
+++ inventory/migrations/0001_initial.py
@@ -14,25 +14,5 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('when', models.TimeField(auto_now_add=True, verbose_name=b'date created')),
             ],
-        ),
-        migrations.CreateModel(
-            name='Ingredient',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Ingredient')),
-            ]
-        ),
-        migrations.CreateModel(
-            name='MenuCategory',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Type of Menu Food')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='MenuItem',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Menu Item')),
-                ('ingredients', models.ManyToManyField(to='Ingredient', verbose_name='list of ingredients')),
-                ('category', models.ForeignKey(to='MenuCategory', verbose_name='category this belongs to', on_delete=models.CASCADE))
-            ],
         )
     ]
