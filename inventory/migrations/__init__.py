02f3a26d691a667ebb5c6c3105ae208ef5a57294
satvu <sarahvu@mit.edu>
2020-01-22 14:02:23 -0600

    empty migration

--- inventory/migrations/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.db import migrations, models
-
-class Migration(migrations.Migration):
-
-    dependencies = [('migrations', '0001_initial')]
-
-    operations = [
-    ]
