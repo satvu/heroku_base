4bf90e35b57792cf660af77fcbccc6e5a036321e
satvu <sarahvu@mit.edu>
2020-01-22 08:23:17 -0600

    try migraiton file again

--- inventory/migrations/0001_initial.py
+++ inventory/migrations/0001_initial.py
@@ -4,5 +4,20 @@ class Migration(migrations.Migration):
 
     dependencies = []
 
-    operations = [
+    operations = [        
+        migrations.CreateModel(
+            name='Greeting',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('when', models.TimeField(auto_now_add=True, verbose_name=b'date created')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Ingredient',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=30, auto_created=True, verbose_name=b'ingredient name')),
+                ('amount', models.IntegerField(auto_created=True))
+            ]
+        )
     ]
