75a0b7f8f303a8330c876eda726f66970e9383d0
satvu <sarahvu@mit.edu>
2020-01-22 17:00:38 -0600

    add menu category

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class MenuCategory(admin.ModelAdmin):
+    list_display = ('name')
+
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'amount')
 
@@ -9,4 +12,5 @@ class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
 
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(MenuItem)
+admin.site.register(MenuCategory, MenuCategoryAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
