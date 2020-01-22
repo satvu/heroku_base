45191a0059c499e22851e70e45b721842adfad3f
satvu <sarahvu@mit.edu>
2020-01-22 17:02:47 -0600

    edit

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class MenuCategory(admin.ModelAdmin):
+class MenuCategoryAdmin(admin.ModelAdmin):
     list_display = ('name')
 
 class IngredientAdmin(admin.ModelAdmin):
