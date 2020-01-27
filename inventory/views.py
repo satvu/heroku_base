b030dff71c61361a6583958b3dfdd445b0101c0c
satvu <sarahvu@mit.edu>
2020-01-27 14:14:20 -0600

    import everything in views

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting, MenuItem
+from .models import *
 
 # Create your views here.
 def index(request):
