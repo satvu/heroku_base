035024520f7abdbb5bb9ce805c965c827f691f4c
satvu <sarahvu@mit.edu>
2020-01-29 21:55:00 -0600

    wip of email and deleting orders

--- inventory/views.py
+++ inventory/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
 from .models import *
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
     holidays = Holiday.objects.all()
     return render(request, "index.html", {"holidays": holidays})
-
 
 def db(request):
 
@@ -27,3 +26,30 @@ def menu(request):
         menu_dictionary[category.name] = list(menuItems)
 
     return render(request, "menu.html", {"menu_dictionary": menu_dictionary})
+
+def active_orders(request):
+    user = User.objects.get(username=request.user.username)
+    if user.is_staff:
+        if request.method == 'POST':
+            carts = Cart.objects.filter(active = True)
+            ordered_by = 
+            send_mail(
+                '[The Container] Your order is ready!',
+                'Please come pick up your order as soon as possible.',
+                user.email,
+                [],
+                fail_silently=False,
+            )
+            return render(request, 'active_orders.html', {"carts": carts, })
+
+        else:
+            active_carts = Cart.objects.filter(active = True)
+            cart_orders = dict()
+            for cart in active_carts:
+                cart_orders[active_carts.who_id] =  list(Order.objects.filter(cart_id = cart.id))
+
+            return render(request, 'active_orders.html', {"carts": cart_orders})
+
+    else:
+        raise Http404("You must be a staff member to access this page")
+
